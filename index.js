70cda4bdac82d65cc50133d174e7b83cc51f28e4
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-11-05 14:34:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,13 @@ class Block {
 
 class Enemy extends Block {
     constructor(type) {
-        super(x, y);
+        this.x = 100;
+        this.y = 100;
         this.type = type;
         this.health = 0;
         this.damage = 0;
         this.attackSpeed = 0;
-        this.maxspeed = 0;
+        this.speed = 0;
         this.distanceCap = 0;
         this.setEverything();
     }
@@ -42,16 +43,16 @@ class Enemy extends Block {
                 this.health = 1;
                 this.damage = 20;
                 this.attackSpeed = 1000;
-                this.maxspeed = -10;
+                this.speed = -10;
                 this.distanceCap = 100;
                 this.color = 'green';
                 break;
             case 1:
                 //for chonk
-                this.heath = 20;
+                this.heath = 10;
                 this.damage = 40;
                 this.attackSpeed = 3000;
-                this.maxspeed = -1;
+                this.speed = -1;
                 this.distanceCap = 100;
                 this.width = this.width * 1.5;
                 this.height = this.height * 1.5;
@@ -62,7 +63,7 @@ class Enemy extends Block {
                 this.health = 2;
                 this.damage = 20;
                 this.attackSpeed = -1000;
-                this.maxspeed = 3;
+                this.speed = -3;
                 this.distanceCap = 1000;
                 this.color = 'purple';
                 break;
@@ -71,7 +72,7 @@ class Enemy extends Block {
                 this.health = 1;
                 this.damage = 10;
                 this.attackSpeed = -2500;
-                this.maxspeed = -2;
+                this.speed = -2;
                 this.distanceCap = 25;
                 this.color = 'grey';
                 
@@ -81,11 +82,12 @@ class Enemy extends Block {
                 this.health = 1;
                 this.damage = 50;
                 this.attackSpeed = 3000;
-                this.maxspeed = -5;
+                this.speed = -5;
                 this.distanceCap = 25;
                 this.color = 'red';
                 break;
         }
+        this.setEverything();
         this.draw();
     }
     
@@ -99,24 +101,55 @@ function animate() {
     block.draw();
 }
 
+function checkHealth(){
+    for(let i=0; i<enemiesAll.length; ++i){
+        if(enemiesAll[i].health <= 0){
+            delete enemiesAll[i];
+            //blow up and then thomas_death.mp4
+        }
+    }
+}
+
 function detectEnemies(e){
     let mouseX = e.clientX;
     let mouseY = e.clientY;
     for(let i = 0; i < enemiesAll.length; ++i){
-        if((mouseX <= enemiesAll[i].x + enemiesAll[i].width) && mouseX >= enemiesAll[i].x && mouseY <= )
+        if(mouseX <= enemiesAll[i].x + enemiesAll[i].width && mouseX >= enemiesAll[i].x && mouseY <= enemiesAll[i].y + enemiesAll[i].height && mouseY >= enemiesAll[i].y){
+            enemiesAll[i].health--;
+            checkHealth();
+        }
     }
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+function newEnemy(){
+    let type = getRandomInt(0, 5);
+    let newEnemy = new Enemy(type);
+    enemiesAll.push(newEnemy);
+}
+
+function playGame(){
+    var newEnemy = window.setInterval(newEnemy, 100); //Create new enemy every 3 seconds
+    newEnemy.y = getRandomInt%200;
+    newEnemy.x = window.innerWidth;
+}
 
 window.onload = function(){
     document.addEventListener('click', (e) =>{
         console.log("Mouse X position: " + e.clientX + "\nMouse Y position: " + e.clientY);
         detectEnemies();
     });
+    playGame();
 }
 
 
 
 let block = new Block(canvas.width / 2, canvas.height / 2);
+let enemy = new Enemy(3);
 animate();
 
